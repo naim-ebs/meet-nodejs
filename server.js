f42dab70b7c194157fb0c68071bc43d3205cb26c
Naim Siddiqui <Naim@Shudiptos-Mac-mini.local>
2026-01-19 12:16:20 +0600

    Module-04, Video-003: Setup Socket at Client Site

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ var server = app.listen(3000, function () {
     console.log("Listening on port 3000");
 });
 
-const io = require("socket.io")(server);
+const io = require("socket.io")(server, {
+    allowEIO3: true
+});
 app.use(express.static(path.join(__dirname, "")));
 
 io.on("connection", (socket) => {
-    console.log("a user connected: " + socket.id);
+    console.log("socket id is ", socket.id);
 });
