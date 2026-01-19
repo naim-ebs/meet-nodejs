4b3aaea0c84b45c4cf59cf0c4638d8f8e736b087
Naim Siddiqui <Naim@Shudiptos-Mac-mini.local>
2026-01-19 13:33:49 +0600

    Module-05, Video-001: Let Other User Know About Me

--- public/Assets/js/app.js
+++ public/Assets/js/app.js
@@ -1,13 +1,27 @@
 var MyApp = (function () {
+    var socket = null;
+    var user_id = "";
+    var meeting_id = "";
     function init(uid, mid) {
+        user_id = uid;
+        meeting_id = mid;
         event_process_for_signaling_server();
     }
 
-    var socket = null;
     function event_process_for_signaling_server() {
-        socket = io();
+        socket = io.connect();
         socket.on("connect", () => {
-            alert("socket connected to client side");
+            if (socket.connected) {
+                if (user_id != "" && meeting_id != "") {
+                    socket.emit("userconnect", {
+                        displayName: user_id,
+                        meetingid: meeting_id
+                    });
+                }
+            }
+        });
+        socket.on("inform_others_about_me", function (data) {
+            addUser(data.other_user_id, data.connId);
         });
     }
 
--- server.js
+++ server.js
@@ -12,4 +12,21 @@ app.use(express.static(path.join(__dirname, "")));
 
 io.on("connection", (socket) => {
     console.log("socket id is ", socket.id);
+    socket.on("userconnect", (data) => {
+        console.log("userconnect", data.displayName, data.meetingId);
+        var other_users = userConnections.filter(
+            (p) => p.meeting_id === data.meetingId
+        );
+        userConnections.push({
+            connectionId: socket.id,
+            user_id: data.displayName,
+            meeting_id: data.meetingId,
+        });
+        other_users.forEach((v) => {
+            socket.to(v.connectionId).emit("inform_others_about_me", {
+                other_user_id: data.displayName,
+                connId: socket.id,
+            })
+        })
+    });
 });
