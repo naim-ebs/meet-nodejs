2c87599f154ab561a170d79907fac608c9a1bfa4
Naim Siddiqui <Naim@Shudiptos-Mac-mini.local>
2026-01-22 18:14:50 +0600

    Module-09, Video-01: Creating leave meeting functionality

--- public/Assets/js/app.js
+++ public/Assets/js/app.js
@@ -494,14 +494,14 @@ var MyApp = (function () {
         $(".g-right-details-wrap").addClass("panel-hidden");
         return false;
     });
-    
+
     $(document).on("click", ".meeting-heading-cross span", function (e) {
         e.stopPropagation();
         e.preventDefault();
         $(".g-right-details-wrap").addClass("panel-hidden");
         return false;
     });
-    
+
     $(document).on("click", ".meeting-details-button", function (e) {
         e.stopPropagation();
         $(".g-right-details-wrap").removeClass("panel-hidden");
@@ -522,7 +522,30 @@ var MyApp = (function () {
         $(".chat-heading").addClass("active");
         $(".people-heading").removeClass("active");
     });
-    
+
+    $(document).on("click", ".end-call-wrap", function () {
+        $(".top-box-show")
+            .css({
+                display: "block",
+            })
+            .html(
+                '<div class="top-box align-vertical-middle profile-dialogue-show"> <h1 class="mt-2" style="text-align:center; color:white;">Leave Meeting</h1> <hr> <div class="call-leave-cancel-action d-flex justify-content-center align-items-center w-100"> <a href="/action.html"><button class="call-leave-action btn btn-danger mr-5">Leave</button></a> <button class="call-cancel-action btn btn-secondary">Cancel</button> </div> </div>'
+            );
+    });
+
+    $(document).mouseup(function (e) {
+        var container = new Array();
+        container.push($(".top-box-show"));
+        $.each(container, function (key, value) {
+            if (!$(value).is(e.target) && $(value).has(e.target).length == 0) {
+                $(value).empty();
+            }
+        })
+    })
+
+    $(document).on("click", ".call-cancel-action", function () {
+        $('.top-box-show').html('');
+    })
 
     return {
         _init: function (uid, mid) {
